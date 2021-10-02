17fd1cdc352ffaf0828d843037de684820fea78f
romanocast <castagnoli@simpit.ch>
2021-10-02 13:32:23 +0200

    initialcommit

---
+++ snakeCopy/src/GamePanel.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener  {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
